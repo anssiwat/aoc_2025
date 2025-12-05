78fbd0f57a281c4e60d238633adfafbf3add204b
anssiwat <anssi.wathen@proton.me>
2025-12-05 08:52:54 +0200

    Day 1 part 2 and refactor part 1 one a bit.

--- aoc_2025/Day1.fs
+++ aoc_2025/Day1.fs
@@ -12,20 +12,20 @@ module Day01 =
 
     let parseClicks (input: string) : int =
         let dir = input[0]
-        let dist = rounderModulo (int input[1..])
+        let dist = int input[1..]
 
         match dir with
         | 'L' -> -1 * dist
         | _ -> dist
 
-    let GetResult (start: int, clicks: int list) : int =
+    let GetResultPart1 (start: int, clicks: int list) : int =
         let initialState = (start, 0)
 
         let (finalPosition, finalCounter) =
             clicks
             |> List.fold
                 (fun (currentPos, counter) click ->
-                    let newPos = rounderModulo (currentPos - click)
+                    let newPos = rounderModulo (currentPos - rounderModulo (click))
 
                     let newCounter = if newPos = 0 then counter + 1 else counter
 
@@ -34,23 +34,51 @@ module Day01 =
 
         finalCounter
 
+    let GetResultPart2 (start: int, clicks: int list) : int =
+        let initialState = (start, 0)
+
+        let (finalPosition, finalCounter) =
+            clicks
+            |> List.fold
+                (fun (currentPos, counter) click ->
+                    let newPos = rounderModulo (currentPos + rounderModulo (click))
+                    let orbits = abs (int click / 100)
+
+                    let passed: bool =
+                        currentPos + (click % 100) >= 100
+                        || currentPos + (click % 100) <= 0 && currentPos <> 0
+
+                    let total_orbits = orbits + (if passed then 1 else 0)
+                    let newCounter = counter + total_orbits
+
+                    (newPos, newCounter))
+                initialState
+
+        finalCounter
+
     let solveExample () =
         let text: string list = Helpers.readLinesFromFile examplePath
-        // let movements: int list = text |> List.map parseClicks
         let clicks = text |> List.map parseClicks
 
-        let result = GetResult(50, clicks)
+        let result = GetResultPart2(50, clicks)
         result
 
     let solvePart1 () =
         let text: string list = Helpers.readLinesFromFile inputPath
-        // let movements: int list = text |> List.map parseClicks
         let clicks = text |> List.map parseClicks
 
-        let result = GetResult(50, clicks)
+        let result = GetResultPart1(50, clicks)
+        result
+
+    let solvePart2 () =
+        let text: string list = Helpers.readLinesFromFile inputPath
+        let clicks = text |> List.map parseClicks
+
+        let result = GetResultPart2(50, clicks)
         result
 
     let run () =
-        // Call the function with () and print the returned integer
         let resultOfPart1 = solvePart1 ()
         printfn "The final result of Part 1 is: %d" resultOfPart1
+        let resultOfPart2 = solvePart2 ()
+        printfn "The final result of Part 2 is: %d" resultOfPart2
--- aoc_2025/Helpers.fs
+++ aoc_2025/Helpers.fs
@@ -13,22 +13,3 @@ let readLinesFromFile (filePath: string) : string list =
     | ex ->
         printfn "An unexpected error occurred: %s" ex.Message
         []
-
-
-// let path = "movements.txt"
-// let content = [
-//     "L68"; "L30"; "R48"; "L5"; "R60";
-//     "L55"; "L1"; "L99"; "R14"; "L82"
-// ]
-// File.WriteAllLines(path, content)
-//
-// // 2. Call the function with the path to your file.
-// let movements = readLinesFromFile path
-//
-// // 3. Print the results to verify.
-// printfn "Successfully read the following lines from the file:"
-// movements |> List.iter (fun line -> printfn "'%s'" line)
-//
-// // Example of what the 'movements' list will contain:
-// // val movements : string list =
-// //   ["L68"; "L30"; "R48"; "L5"; "R60"; "L55"; "L1"; "L99"; "R14"; "L82"]
